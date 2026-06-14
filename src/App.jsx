8a6b72e752e7849e588eb07ca1b1a37c5d468def
Gajerla Praneeth <buntypraneeth012@gmail.com>
2026-06-15 03:41:51 +0530

    counter app created first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import "./App.css";
+
+const App = () => {
+  const [count,setcount]=useState(0);
+  const increase=()=>{
+setcount(count+1)
+  };
+  const decrease=()=>{
+    setcount(count-1)
+  };
+  const reset=()=>{
+setcount(0)
+  };
+  return (
+    <div className='app'>
+      <div className='card'>
+<h1> React counter app</h1>
+<h2>{count}</h2>
+<div className='buttons'>
+<button onClick={decrease}>-</button>
+<button onClick={reset}>Reset</button>
+<button onClick={increase}>+</button>
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
